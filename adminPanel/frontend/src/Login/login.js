7ca3ae3e0e431212fcbe3310afe733d72c30337c
mleitejens <mleitejensen@gmail.com>
2023-12-08 14:27:32 +0100

    Set up the base frontend and backend for the admin panel

---
+++ adminPanel/frontend/src/Login/login.js
@@ -0,0 +1,18 @@
+import "./login.css"
+
+function Login(){
+    return(
+        <div className="login">
+            <form>
+                <div className="formTitle">Login</div>
+                <label for="">Username</label><br/>
+                <input type="text"></input><br/>
+                <label>Password</label><br/>
+                <input type="password"></input><br/>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
---
+++ adminPanel/frontend/src/Signup/signup.js
@@ -0,0 +1,18 @@
+import "./signup.css"
+
+function Signup(){
+    return(
+        <div className="signup">
+            <form>
+                <div className="formTitle">Sign up</div>
+                <label for="">Username</label><br/>
+                <input type="text"></input><br/>
+                <label>Password</label><br/>
+                <input type="password"></input><br/>
+                <button type="submit">Sign up</button>
+            </form>
+        </div>
+    )
+}
+
+export default Signup
