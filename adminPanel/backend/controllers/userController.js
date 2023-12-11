6f7797cd8db4db7f21e11b4dc34487cf43c2cc50
Martin <marekatt2004@gmail.com>
2023-12-11 12:54:16 +0100

    Added error message when signing up

--- adminPanel/backend/controllers/userController.js
+++ adminPanel/backend/controllers/userController.js
@@ -3,24 +3,19 @@ const mongoose = require("mongoose")
 
 const getUsers = async (req, res) => {
   const users = await User.find({}).sort({username: +1})
-  
   res.status(200).json(users)
 }
 
 const createUser = async (req, res) => {
-  
-  const { username, password, admin } = req.body;
+  const { username, password } = req.body;
 
-  try {
-    const user = await User.create({ username, password, admin });
-    res.status(201).json({ user: user._id });
-  }
-  catch(err) {
-    console.log(err)
-    res.status(400).json({ err });
+  try{
+    const user = await User.signup(username, password)
+    res.status(200).json({ user })
+  }catch (error) {
+    res.status(400).json({error: error.message})
   }
 }
-
 
 module.exports = {
     getUsers,
--- adminPanel/frontend/src/components/signup.js
+++ adminPanel/frontend/src/components/signup.js
@@ -6,28 +6,37 @@ const Signup = () => {
     const [password, setPassword] = useState('')
     const [error, setError] = useState(null)
     const [isLoading, setIsLoading] = useState(null)
+    const [result, setResult] = useState('')
     
     const handleSubmit = async (e) => {
         e.preventDefault()
         setIsLoading(true)
         setError(null)
+        setResult('')
     
 
         const response = await fetch('http://localhost:9000/api/create',{
             method: 'POST',
             headers: {'Content-Type': 'application/json',},
-            body: JSON.stringify({ username, password, admin: "no" })
+            body: JSON.stringify({ username, password})
         })
         const json = await response.json()
 
         if (!response.ok) {
         setIsLoading(false)
         setError(json.error)
+        console.log(json.error)
         }
         if (response.ok) {
-        // update loading state
         setIsLoading(false)
+        setResult("User created")
         }
+        
+        const removeResult = (() => {
+            setResult('')
+        })
+
+        setTimeout(removeResult(), 2000)
     }
 
     return(
@@ -35,12 +44,14 @@ const Signup = () => {
             <form onSubmit={handleSubmit}>
                 <div className="formTitle">Sign up</div>
                 <label>Username</label><br/>
-                <input type="text" name="username" onChange={(e) => setUsername(e.target.value)} required></input><br/>
+                <input type="text" name="username" onChange={(e) => setUsername(e.target.value)}></input><br/>
                 <label>Password</label><br/>
-                <input type="password" name="password" onChange={(e) => setPassword(e.target.value)} required></input><br/>
+                <input type="password" name="password" onChange={(e) => setPassword(e.target.value)}></input><br/>
 
                 <button disabled={isLoading}>Sign up</button>
                 {error && <div className="error">{error}</div>}
+                {result && <div className="result">{result}</div>}
+
             </form>
         </div>
     )
