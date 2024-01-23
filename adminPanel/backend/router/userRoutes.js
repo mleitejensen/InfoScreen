5778fc3b519ee52e382fe359d7ad7cf725be3732
Martin <marekatt2004@gmail.com>
2024-01-23 13:09:13 +0100

    Added auth to routes in backend

--- adminPanel/backend/router/userRoutes.js
+++ adminPanel/backend/router/userRoutes.js
@@ -1,12 +1,15 @@
 const { Router } = require('express');
 const router = Router()
+const requireAuth = require("../middleware/requireAuth")
 const { getUsers, createUser, loginUser, deleteUser, updateUser } = require("../controllers/userController")
 
+
+router.post("/signup", createUser)
+router.post("/login", loginUser)
+router.use(requireAuth)
 router.get("/users", getUsers)
 router.post("/users/delete", deleteUser)
 router.post("/users/update", updateUser)
-router.post("/signup", createUser)
-router.post("/login", loginUser)
 
 
 module.exports = router
